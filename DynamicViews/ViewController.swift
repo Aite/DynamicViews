032c4085cfc44df320251b3741488bc247a9a608
Alaa Alzaibak <alaa@voltlines.com>
2020-06-10 09:33:44 +0300

    Initial Commit

---
+++ DynamicViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicViews
+//
+//  Created by Alaa AlZaibak on 10.06.2020.
+//  Copyright © 2020 Volt Lines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
